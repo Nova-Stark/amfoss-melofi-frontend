f29cb40525036b9bffe9282349078301b9fa4c89
NovaStark <nova2023ai@gmail.com>
2025-11-20 21:11:57 +0530

    Login/background

---
+++ src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import './Login.page.css';
+
+export default function Login() {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <p>Please enter your credentials to log in.</p>
+    </div>
+  );
+}
