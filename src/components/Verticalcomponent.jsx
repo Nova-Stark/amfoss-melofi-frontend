c234d548f1cf4d911a4cb353e42f38eef57ec1ad
NovaStark <nova2023ai@gmail.com>
2025-11-25 11:20:43 +0530

    vc/2/hc

--- src/components/Verticalcomponent.jsx
+++ src/components/Verticalcomponent.jsx
@@ -1,18 +1,26 @@
 import styles from './Verticalcomponent.module.css'
 import thumbnailpng from '../assets/thumbnail.jpg'
 
-export default function Verticalcomponent ({recentplaylist = [{id:'songid1',title:'songname1',thumbnail:thumbnailpng},
+export default function Verticalcomponent ({recentplaylist = [
+    {id:'songid1',title:'songname1',thumbnail:thumbnailpng},
     {id:'songid2',title:'songname2',thumbnail:thumbnailpng},
-    {id:'songid3',title:'songname3',thumbnail:thumbnailpng}
+    {id:'songid3',title:'songname3',thumbnail:thumbnailpng},
+    {id:'songid4',title:'songname4',thumbnail:thumbnailpng},
+    {id:'songid5',title:'songname5',thumbnail:thumbnailpng},
+    {id:'songid6',title:'songname6',thumbnail:thumbnailpng},
+    {id:'songid7',title:'songname7',thumbnail:thumbnailpng},
+    {id:'songid8',title:'songname8',thumbnail:thumbnailpng},
+    {id:'songid9',title:'songname9',thumbnail:thumbnailpng},
+    {id:'songid10',title:'songname10',thumbnail:thumbnailpng}
 ]}){
     return (
         <div className={styles['vertical-section']}>
 
             <div className={styles['vertical-component']}>
                 {recentplaylist.map((item) => (
-                    <div id={item.id} className={styles['recent-song']}>
+                    <div id={item.id} key={item.id} className={styles['recent-song']}>
                         <div className={styles['song-img']}>
-                            <img src={item.thumbnail} alt={thumbnailpng} />
+                            <img src={item.thumbnail} alt={item.title} />
                         </div>
                         <div className={styles['song-title']}>
                             {item.title}
